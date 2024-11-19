1915cf7cf9f230df6dd6d706c3378b9e2d780ab9
Fermin Jimenez <ferminjimenez93@gmail.com>
2024-11-19 22:01:33 +0100

    add CreateCompanyDto and implement createCompany method in CompaniesService

--- api/src/company/companies.service.ts
+++ api/src/company/companies.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateCompanyDto } from './dtos/create-company.dto';
 
 @Injectable()
-export class CompaniesService {}
+export class CompaniesService {
+    constructor(@InjectModel('companies') private readonly companyModel) {}
+    
+    async createCompany(createCompanyDto: CreateCompanyDto) {
+        const company = new this.companyModel(createCompanyDto);
+        return company.save();
+    }
+}
