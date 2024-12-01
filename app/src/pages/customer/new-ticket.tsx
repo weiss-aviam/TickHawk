6767cca1f836563b92132f28f641e21e2e45d548
Fermin Jimenez <ferminjimenez93@gmail.com>
2024-12-01 20:55:05 +0100

    feat: add default option to department selection in new ticket form

--- app/src/pages/customer/new-ticket.tsx
+++ app/src/pages/customer/new-ticket.tsx
@@ -123,6 +123,7 @@ function NewTicket () {
                       <div className='inline-flex items-center'>
                         <div className='flex-1 min-w-0'>
                           <select className='bg-gray-50 border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'>
+                            <option>Select department</option>
                             {departments.map((department: any) => (
                                 <option key={department._id}>{department.name}</option>
                             ))}
