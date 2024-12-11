bc1a8f024911aa02c25a56b53d4febb40e15a593
Fermin Jimenez <ferminjimenez93@gmail.com>
2024-12-11 22:12:36 +0100

    feat: integrate DateFormat component for ticket update timestamp and remove unused code

--- app/src/pages/customer/ticket.tsx
+++ app/src/pages/customer/ticket.tsx
@@ -1,4 +1,5 @@
 import { useAuth } from 'components/AuthProvider'
+import DateFormat from 'components/DateFormat'
 import StatusBadge from 'components/StatusBadge'
 import TicketComment from 'components/TicketComment'
 import TimeFormat from 'components/TimeFormat'
@@ -157,7 +158,9 @@ function Ticket () {
                       <div className='inline-flex items-center'>
                         <div className='flex-1 min-w-0'>
                           <span className='block text-base text-gray-900 truncate dark:text-white'>
-                            10-03-2023 12:45 PM
+                            {(ticket && ticket?.updatedAt) && (
+                              <DateFormat date={ticket?.updatedAt} />
+                            )}
                           </span>
                         </div>
                       </div>
@@ -209,41 +212,6 @@ function Ticket () {
                             )}
                           </span>
                         </div>
-                      </div>
-                    </div>
-                  </li>
-
-                  <li className='py-4 hidden'>
-                    <div className='flex items-center space-x-4'>
-                      <div className='flex-shrink-0'>
-                        <svg
-                          className='w-5 h-5 dark:text-white'
-                          aria-hidden='true'
-                          focusable='false'
-                          data-prefix='fab'
-                          data-icon='twitter'
-                          role='img'
-                          xmlns='http://www.w3.org/2000/svg'
-                          viewBox='0 0 512 512'
-                        >
-                          <path
-                            fill='currentColor'
-                            d='M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z'
-                          ></path>
-                        </svg>
-                      </div>
-                      <div className='flex-1 min-w-0'>
-                        <span className='block text-base font-semibold text-gray-900 truncate dark:text-white'>
-                          Twitter account
-                        </span>
-                        <span className='block text-sm font-normal truncate text-primary-700 hover:underline dark:text-primary-500'>
-                          www.twitter.com/themesberg
-                        </span>
-                      </div>
-                      <div className='inline-flex items-center'>
-                        <span className='px-3 py-2 mb-3 mr-3 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-primary-300 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700'>
-                          Disconnect
-                        </span>
                       </div>
                     </div>
                   </li>
